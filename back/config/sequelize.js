463d21cedd5fce27f155e92495fbc8c52d858440
Jérémie Fournier <jeremie.fournier@hotmail.com>
2024-06-03 14:41:50 +0200

    modification mise en ligne

--- back/config/sequelize.js
+++ back/config/sequelize.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-// Initialisation de Sequelize avec les informations de connexion à la base de données
-const sequelize = new Sequelize('connect', 'root', '', {
-  host: 'localhost', // Adresse du serveur MySQL
-  dialect: 'mysql' // Type de base de données utilisée (MySQL)
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  }
+);
 
-// Exportation de l'instance Sequelize pour être utilisée dans d'autres parties de l'application
+// Export the Sequelize instance for use in other parts of the application
 module.exports = sequelize;
